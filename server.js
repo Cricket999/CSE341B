194621b26185a6c14ae93fd307af5aef57b9ff79
Cricket999 <danieltbeatty9@gmail.com>
2023-10-26 01:56:47 -0600

    Basic OAUTH login system

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { auth, requiresAuth, requiredScopes } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+const port = 3000;
 
 const app = express();
 
-const port = 3000;
+app.use(auth(config));
 
 app.use(bodyParser.json())
 app.use("/", require("./routes"));
